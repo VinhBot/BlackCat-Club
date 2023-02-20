2df4545cbeabcced812310f1cd055a3fe9bfac29
Nguyễn Vinh <115027531+VinhBot@users.noreply.github.com>
2023-02-20 09:32:13 +0000

    cập nhật lại preloader css sang scss

--- src/asset/styles/styles.js
+++ src/asset/styles/styles.js
@@ -4,5 +4,4 @@ import "./grid.css";
 import "./main.scss";
 import "./style.scss";
 import "./responsive.scss";
-import "./preloader.css";
 document.cookie = "SameSite=None";
--- src/components/loading/LoadingSvg.js
+++ src/components/loading/LoadingSvg.js
@@ -1,5 +1,4 @@
 import React, { memo } from "react";
-import "./loading.css";
 
 const LoadingSvg = memo(({ isLoadMore }) => {
    return (
